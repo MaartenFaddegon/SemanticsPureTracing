521ed1be6d0a1c3022464f34d4220d8bc5aef875
Maarten Faddegon <mf357@kent.ac.uk>
2015-04-21 10:42:10 +0100

    reorganise

---
+++ src/Examples.hs
@@ -0,0 +1,369 @@
+module Examples where
+import Prelude hiding (Right)
+import Data.Graph.Libgraph
+import Semantics
+
+-- We will use the following constructor encoding in the examples:
+c_0 :: [Name] -> Expr
+c_0 = Constr (ConstrId 0) -- False
+
+c_1 :: [Name] -> Expr
+c_1 = Constr (ConstrId 1) -- True
+
+c_2 :: [Name] -> Expr
+c_2 = Constr (ConstrId 2) -- the empty list []
+
+c_3 :: [Name] -> Expr
+c_3 = Constr (ConstrId 3) -- the list constructor (:)
+
+
+-- NOTE: this only traces the top, subsequent (recursive) calls to foldl are not traced
+foldlTraced :: Expr -> Expr
+foldlTraced = Let ("foldlT", Lambda "f" $ Lambda "xs"  $ Lambda "z"
+                           $ Apply (Apply (Apply (Observe "foldl" Right
+                             (Var "foldl")) "f") "xs" ) "z")
+
+sumTraced :: Expr -> Expr
+sumTraced = Let ("sum", Lambda "xs" (Apply (Observe "sum" Right (Lambda "xs'" 
+                        (Apply (Apply (Apply (Var "foldl") "p") "0" )"xs'"))) "xs"))
+
+mapTraced :: Expr -> Expr
+mapTraced = Let ("mapT", Lambda "f" $ Lambda "xs"
+                       $ Apply (Apply (Observe "map" Right (Var "map")) "f") "xs" )
+
+-- data B = T | F
+-- n :: B -> B
+myNot :: Expr -> Expr
+myNot     = Let ("n", Lambda "b" $ Case (Var "b")
+                    [ (c_1 [], c_0 [])
+                    , (c_0 [], c_1 [])
+                    ])
+
+notTraced :: Expr -> Expr
+notTraced = Let ("n", Lambda "b'" $ Apply (Observe "n" Right $ Lambda "b"
+                      $ Case (Var "b")
+                             [ (c_1 [], c_0 [])
+                             , (c_0 [], c_1 [])
+                             ]) "b'")
+
+
+-- x :: B -> B -> B
+myXor :: Expr -> Expr
+myXor = Let ("x", Lambda "a1" $ Lambda "a2" $ Apply (Apply (Observe "x" Right $ Lambda "b1" $ Lambda "b2"
+                      $ Case (Var "b1")
+                             [ (c_1 [], Case (Var "b2")[ (c_1 [], c_0 [])
+                                                              , (c_0 [], c_1 [])])
+                             , (c_0 [], Case (Var "b2")[ (c_1 [], c_1 [])
+                                                              , (c_0 [], c_1 [])])
+                             ]
+                             ) "a1") "a2")
+
+
+ex1 :: Expr
+ex1 = {- import -} prelude
+    $ {- import -} notTraced
+    $ Let ("b", (c_0 []))
+    $ Print $ Apply (Var "n") "b"
+
+-- Example 2: Function h and n are traced, h maps over a list and n is
+-- applied to all elements.
+--
+--   a) without reverse
+--   b) reverse before printing (but outside traced code)
+ex2a :: Expr
+ex2a = {- import -} prelude
+     $ {- import -} notTraced
+     $ Let ("xs", Let ("a", c_1 [])
+                $ Let ("b", c_0 [])
+                $ Let ("c2", c_2 [])
+                $ Let ("c1", c_3 ["b", "c2"])
+                $            c_3 ["a","c1"])
+     $ Let ("h", Lambda "xs" (Apply (Observe "h" Right (Apply (Var "map") "n")) "xs"))
+     $ Print $ Apply (Var "h") "xs"
+
+ex2b :: Expr
+ex2b = {- import -} prelude
+     $ {- import -} notTraced
+     $ Let ("xs", Let ("a", c_1 [])
+                $ Let ("b", c_0 [])
+                $ Let ("c2", c_2 [])
+                $ Let ("c1", c_3 ["b", "c2"])
+                $            c_3 ["a","c1"])
+     $ Let ("h", Lambda "xs" (Apply (Observe "h" Right (Apply (Var "map") "n")) "xs"))
+     $ Print $ Let ("ys", Apply (Var "h") "xs") 
+             $ Apply (Var "reverse") "ys"
+
+-- Example 3: Trace map and its callees, reverse result before printing
+-- but outside traced code
+ex3a :: Expr
+ex3a = {- import -} prelude
+     $ {- import -} myNot
+     $ {- import -} mapTraced
+     $ Let ("xs", Let ("a", c_1 [])
+                $ Let ("b", c_0 [])
+                $ Let ("c2", c_2 [])
+                $ Let ("c1", c_3 ["b", "c2"])
+                $            c_3 ["a","c1"])
+     $ Print $ Let ("ys", Apply (Apply (Var "mapT") "n") "xs")
+             $ Var "ys"
+
+ex3b :: Expr
+ex3b = {- import -} prelude
+     $ {- import -} notTraced
+     $ {- import -} mapTraced
+     $ Let ("xs", Let ("a", c_1 [])
+                $ Let ("b", c_0 [])
+                $ Let ("c2", c_2 [])
+                $ Let ("c1", c_3 ["b", "c2"])
+                $            c_3 ["a","c1"])
+     $ Print $ Let ("ys", Apply (Apply (Var "mapT") "n") "xs")
+             $ Apply (Var "reverse") "ys"
+
+-- Example 4: Trace foldl and its callees in a simple checksum program
+ex4 :: Expr
+ex4 = {- import -} prelude
+    $ {- import -} foldlTraced
+    $ {- import -} myXor
+     $ Let ("bs", Let ("a", c_1 [])
+                $ Let ("b", c_0 [])
+                $ Let ("c2", c_2 [])
+                $ Let ("c1", c_3 ["b", "c2"])
+                $            c_3 ["a","c1"])
+     $ Let ("z", c_0 [])
+     $ Print $ Apply (Apply (Apply (Var "foldlT") "x") "z") "bs"
+
+-- Example 5: 
+--      a) f -> g -> h
+--      b) f -> g, f -> h
+--      c) a -> b, a -> c, c -> d
+ex5a :: Expr
+ex5a = Let ("h", Observe "h" Right $ Lambda "y" $ Var "y")
+     $ Let ("g", Observe "g" Right $ Lambda "y" $ Apply (Var "h") "y")
+     $ Let ("f", Observe "f" Right $ Lambda "y" $ Apply (Var "g") "y")
+     $ Let ("k", c_1 [])
+     $ Print $ Apply (Var "f") "k"
+
+traceEx5a :: Trace
+traceEx5a = reverse . snd . evaluate $ ex5a
+
+ex5b :: Expr
+ex5b = Let ("h", Observe "h" Right (Lambda "y" $ Var "y"))
+     $ Let ("g", Observe "g" Right (Lambda "y" $ Var "y"))
+     $ Let ("f", Observe "f" Right (Lambda "y" $ Let ("z", Apply (Var "g") "y")
+                                                               (Apply (Var "h") "z")
+                                             ))
+     $ Let ("k", c_1 [])
+     $ Print $ Apply (Var "f") "k"
+
+traceEx5b :: Trace
+traceEx5b = reverse . snd . evaluate $ ex5b
+
+ex5c :: Expr
+ex5c = Let ("mod2",    Observe "mod2"    Right (Lambda "x" $ Var "x"))
+     $ Let ("isEven",  Observe "isEven"  Right (Lambda "x" $ Apply (Var "mod2") "x"))
+     $ Let ("plusOne", Observe "plusOne" Right (Lambda "x" $ Var "x"))
+     $ Let ("isOdd",   Observe "isOdd"   Right (Lambda "x" $ Let ("y", Apply (Var "plusOne") "x")
+                                                                 (Apply (Var "isEven") "y")
+                                               ))
+     $ Let ("k", c_1 [])
+     $ Print $ Apply (Var "isOdd") "k"
+
+
+traceEx5c :: Trace
+traceEx5c = reverse . snd . evaluate $ ex5c
+
+-- Example 6:
+--  There are different computation trees that are sound for algorithmic
+--  debugging. Functions as arguments can be represented in different ways
+--  but that also means that different dependencies are needed for a tree
+--  to be sound. We represent functions as a finite mapping thus we should
+--  create the dependencies that fit that.
+
+ex6 :: Expr
+ex6 = Let ("not",  Observe "not"  Wrong (Lambda "b" $ Case (Var "b") [(c_1 [], c_0 []), (c_0 [], c_1 [])]))
+    $ Let ("app",  Observe "app"  Right (Lambda "f" $ Lambda "x" $ Apply (Var "f") "x"))
+    $ Let ("main", Observe "main" Right (Lambda "x" $ Apply (Apply (Var "app") "not") "x"))
+    $ Let ("y",    c_1 [])
+    $ {-in-} Apply (Var "main") "y"
+
+-- ex7a:
+--   app f x = f (not x)
+--   main = app not True
+ex7a :: Expr
+ex7a = Let ("not",  Lambda "a" $ Apply (Observe "not"  Right (Lambda "b" $ Case (Var "b") [(c_1 [], c_0 []), (c_0 [], c_1 [])])) "a")
+     $ Let ("app",  Observe "app"  Right (Lambda "f" $ Lambda "x" $ Let ("y",Apply (Var "not") "x") 
+                                                                  $ Apply (Var "f") "y"))
+     $ Let ("main", Observe "main" Right (Lambda "x" $ Apply (Apply (Var "app") "not") "x"))
+     $ Let ("y",    c_1 [])
+     $ {-in-} Apply (Var "main") "y"
+
+traceEx7a :: Trace
+traceEx7a = reverse . snd . evaluate $ ex7a
+
+-- ex7b:
+--   app f x = not (f x)
+--   main = app not True
+ex7b :: Expr
+ex7b = Let ("not",  Lambda "a" $ Apply (Observe "not"  Right (Lambda "b" $ Case (Var "b") [(c_1 [], c_0 []), (c_0 [], c_1 [])])) "a")
+     $ Let ("app",  Observe "app"  Right (Lambda "f" $ Lambda "x" $ Let ("y",Apply (Var "f") "x") 
+                                                                  $ Apply (Var "not") "y"))
+     $ Let ("main", Observe "main" Right (Lambda "x" $ Apply (Apply (Var "app") "not") "x"))
+     $ Let ("y",    c_1 [])
+     $ {-in-} Apply (Var "main") "y"
+
+
+traceEx7b :: Trace
+traceEx7b = reverse . snd . evaluate $ ex7b
+
+-- ex7c:
+--   app f x = f x
+--   main = app (app not) True
+ex7c :: Expr
+ex7c = Let ("not",  Lambda "a" $ Apply (Observe "not"  Right (Lambda "b" $ Case (Var "b") [(c_1 [], c_0 []), (c_0 [], c_1 [])])) "a")
+     $ Let ("app", Lambda "g" $ Lambda "y" $ Apply (Apply
+                   (Observe "app"  Right (Lambda "f" $ Lambda "x" $ Apply (Var "f") "x"))
+                   "g" ) "y"
+           )
+     $ Let ("main", Observe "main" Right (Lambda "x" $ Let ("app1", Apply (Var "app") "not") 
+                                                $ Apply (Apply (Var "app") "app1") "x"))
+     $ Let ("y",    c_1 [])
+     $ {-in-} Apply (Var "main") "y"
+
+
+traceEx7c :: Trace
+traceEx7c = reverse . snd . evaluate $ ex7c
+
+-- Example 8: How does our technique handle sharing?
+
+-- How does re-use of the result of "f" affect our dependence inference?
+ex8 :: Expr
+ex8 = genId "f"
+    $ genId "g"
+    $ genId "h"
+    -- p a1 a2 = case a1 of False -> (case a2 of False -> False)
+    $ Let ("p", Lambda "a1" $ Lambda "a2" 
+              $ Apply (Apply (Observe "p" Right
+              $ Lambda "b1" $ Lambda "b2" 
+              $ Case (Var "b1") [(c_0 [], Case (Var "b2") [(c_0 [], c_0 [])])]
+              ) "a1") "a2")
+    -- m a = let k = f a in p (g k) (h k) 
+    $ Let ("m", Lambda "a" (Apply (Observe "m" Right
+          $ Lambda "b"
+          $ Let ("k",  Apply (Var "f") "b")
+          $ Let ("k_g", Apply (Var "g") "k")
+          $ Let ("k_h", Apply (Var "h") "k")
+          $ Apply (Apply (Var "p") "k_g") "k_h"
+          ) "a"))
+    -- main = m False
+    $ Let ("c", c_0 []) $ Apply (Var "m") "c"
+
+  where genId :: String -> Expr -> Expr
+        genId funName = Let (funName, Lambda "x"
+                                    $ Apply 
+                                    ( Observe funName Right
+                                    $ Lambda "y" (Var "y")
+                                    ) "x")
+                                                        
+traceEx8 :: Trace
+traceEx8 = reverse . snd . evaluate $ ex8
+
+-- ex8b is as ex8, but p is not observed
+ex8b :: Expr
+ex8b = genId "f"
+     $ genId "g"
+     $ genId "h"
+     -- p a1 a2 = case a1 of False -> (case a2 of False -> False)
+     $ Let ("p", Lambda "b1" $ Lambda "b2" 
+               $ Case (Var "b1") [(c_0 [], Case (Var "b2") [(c_0 [], c_0 [])])]
+               )
+     -- m a = let k = f a in p (g k) (h k) 
+     $ Let ("m", Lambda "a" (Apply (Observe "m" Right
+           $ Lambda "b"
+           $ Let ("k",  Apply (Var "f") "b")
+           $ Let ("k_g", Apply (Var "g") "k")
+           $ Let ("k_h", Apply (Var "h") "k")
+           $ Apply (Apply (Var "p") "k_g") "k_h"
+           ) "a"))
+     -- main = m False
+     $ Let ("c", c_0 []) $ Apply (Var "m") "c"
+
+  where genId :: String -> Expr -> Expr
+        genId funName = Let (funName, Lambda "x"
+                                    $ Apply 
+                                    ( Observe funName Right
+                                    $ Lambda "y" (Var "y")
+                                    ) "x")
+
+traceEx8b :: Trace
+traceEx8b = reverse . snd . evaluate $ ex8b
+
+-- Example 8c: sharing an argument
+--   f x = x
+--   g x = x
+--   m x = f x + g x
+
+ex8c :: Expr
+ex8c = genId "f"
+     $ genId "g"
+     -- p a1 a2 = case a1 of False -> (case a2 of False -> False)
+     $ Let ("p", Lambda "b1" $ Lambda "b2" 
+               $ Case (Var "b1") [(c_0 [], Case (Var "b2") [(c_0 [], c_0 [])])]
+               )
+     -- m a = let k_f = f a; k_g = g a in p k_f k_g
+     $ Let ("m", Lambda "a" (Apply (Observe "m" Right
+           $ Lambda "b"
+           $ Let ("k_g", Apply (Var "g") "b")
+           $ Let ("k_f", Apply (Var "f") "b")
+           $ Apply (Apply (Var "p") "k_f") "k_g"
+           ) "a"))
+     -- main = m False
+     $ Let ("c", c_0 []) $ Apply (Var "m") "c"
+
+  where genId :: String -> Expr -> Expr
+        genId funName = Let (funName, Lambda "x"
+                                    $ Apply 
+                                    ( Observe funName Right
+                                    $ Lambda "y" (Var "y")
+                                    ) "x")
+
+--------------------------------------------------------------------------------
+-- Prelude, with:
+--
+-- map = \f xs -> case xs of (c_2 [])    -> xs
+--                           (c_3 [h,t]) -> let h' = f h, t' = map f t
+--                                                   in c_3 [h', t']
+--
+-- foldl = \f z xs -> case xs of (c_2 [])    -> z
+--                               (c_3 [h,t]) -> let z' = f z h
+--                                                       in foldl f z' t
+-- reverse = \xs -> let f = \z x -> c_3 [x,z]
+--                      z = c_2 []
+--                  in foldl f z xs
+
+
+prelude :: Expr -> Expr
+prelude e = Let ("map", Lambda "f" $ Lambda "xs" 
+                      $ Case (Var "xs")
+                             [ (c_2 [], Var "xs")
+                             , ( c_3 ["h","t"]
+                               , Let ("h'", Apply (Var "f") "h")
+                               $ Let ("t'", Apply (Apply (Var "map") "f") "t")
+                               $ c_3 ["h'","t'"]
+                               )
+                             ])
+          $ Let ("foldl", Lambda "f" $ Lambda "z"  $ Lambda "xs"
+                        $ Case (Var "xs")
+                             [ (c_2 [], Var "z")
+                             , ( c_3 ["h","t"]
+                               , Let ("z'", Apply (Apply (Var "f") "z") "h")
+                               $ Apply (Apply (Apply (Var "foldl") "f") "z'") "t"
+                               )
+                             ])
+          $ Let ("reverse", Lambda "xs"
+                          $ Let ("f", Lambda "z" $ Lambda "x"
+                                    $ c_3 ["x","z"])
+                          $ Let ("z", c_2 [])
+                          $ Apply (Apply (Apply (Var "foldl") "f") "z") "xs")
+          $ e
+
+
--- src/Test.hs
+++ src/Test.hs
@@ -1,5 +1,112 @@
+import Prelude hiding (Right)
+import Data.List(isPrefixOf,sort)
 import Semantics
+import Examples
 import Test.QuickCheck
+import Control.Monad.State
+import Data.Graph.Libgraph
+
+--------------------------------------------------------------------------------
+-- QuickCheck soundness property
+
+subsetOf :: Ord a => [a] -> [a] -> Bool
+subsetOf xs ys = isPrefixOf (sort xs) (sort ys)
+
+nonEmptyTrace :: Expr -> Bool
+nonEmptyTrace = not . null . snd . evaluate
+
+prop_actuallyFaulty :: Expr -> Property
+prop_actuallyFaulty e = nonEmptyTrace e ==> property $ algoDebug e `subsetOf` markedFaulty e
+
+--------------------------------------------------------------------------------
+-- Generating random expressions with observed abstractions
+
+gen_expr :: Int -> Gen Expr
+gen_expr 0 = gen_constr
+gen_expr n = oneof [ gen_constr
+                   , gen_var
+                   , gen_case n
+                   , liftM2 Lambda gen_varName (gen_expr $ n-1)
+                   , liftM2 Apply  (gen_expr $ n-1) gen_varName
+                   , liftM3 mkLet  gen_varName (gen_expr $ (n-1) `div` 2) (gen_expr $ (n-1) `div` 2)
+                   , gen_observedLam n
+                   ]
+        where mkLet a e1 e2 = Let (a,e1) e2
+
+gen_label :: Gen Label
+gen_label = elements $ map (:[]) ['A'..'Z']
+
+gen_jmt :: Gen Judgement
+gen_jmt = elements [Right, Wrong]
+
+gen_observedLam :: Int -> Gen Expr
+gen_observedLam n = return oLam `ap` gen_label `ap` gen_jmt `ap` gen_varName `ap` (gen_expr $ n-2)
+  where oLam l j v e = Observe l j (Lambda v e)
+
+gen_varName :: Gen String
+gen_varName = elements $ map (:[]) ['a'..'i']
+
+gen_var :: Gen Expr
+gen_var = liftM Var gen_varName
+
+gen_constr :: Gen Expr
+gen_constr = oneof [ elements [c_0 [], c_1 [], c_2 []]
+                   , liftM2 (\v1 v2 -> c_3 [v1,v2]) gen_varName gen_varName
+                   ]
+
+gen_case :: Int -> Gen Expr
+gen_case n = return mkCase `ap` gen_expr' `ap` gen_expr' `ap` gen_expr' `ap` gen_expr' `ap` gen_expr' 
+                           `ap` gen_varName `ap` gen_varName
+  where mkCase e e0 e1 e2 e3 n1 n2 = Case e [(c_0 [],e0),(c_1 [],e1),(c_2 [],e2),(c_3 [n1,n2],e3)]
+        gen_expr' = gen_expr $ (n - 1) `div` 7
+
+uniqueLabels :: Expr -> Expr
+uniqueLabels e = snd (uniqueLabels' lbls e)
+  where lbls = zipWith (++) (cycle ["CC"]) (map show' [1..])
+        show' = show :: Integer -> String
+
+uniqueLabels' :: [Label] -> Expr -> ([Label], Expr)
+uniqueLabels' []   _                     = error "uniqueLabels' exhausted available labels"
+uniqueLabels' lbls (Constr n fs)         = (lbls,Constr n fs)
+uniqueLabels' lbls (Lambda n e)          = let (lbls',e') = uniqueLabels' lbls e
+                                           in (lbls',Lambda n e')
+uniqueLabels' lbls (Apply e n)           = let (lbls',e') = uniqueLabels' lbls e
+                                           in (lbls',Apply e' n)
+uniqueLabels' lbls (Var n)               = (lbls,Var n)
+uniqueLabels' lbls (Let (n,e1) e2)       = let (lbls1,e1') = uniqueLabels' lbls  e1
+                                               (lbls2,e2') = uniqueLabels' lbls1 e2
+                                           in (lbls2,Let (n,e1') e2')
+uniqueLabels' (l:lbls) (Observe _ j e)   = let (lbls',e') = uniqueLabels' lbls e
+                                           in (lbls',Observe l j e')
+uniqueLabels' lbls     (Case e alts)     = let (lbls',alts') = foldl (\(ls,as) alt -> let (ls',a) = uniqueLabels'_tuple ls alt 
+                                                                                      in (ls',a:as)) (lbls,[]) alts
+                                               (lbls'',e')   = uniqueLabels' lbls' e
+                                           in (lbls'',Case e' alts') 
+uniqueLabels' _ expr                      = error $ "Unexpected expr '" ++ show expr ++ "' in uniqueLabels'."
+
+uniqueLabels'_tuple :: [Label] -> (Expr,Expr) -> ([Label], (Expr,Expr))
+uniqueLabels'_tuple ls (e1,e2) = let (ls', e1') = uniqueLabels' ls  e1
+                                     (ls'',e2') = uniqueLabels' ls' e2
+                                 in (ls'', (e1',e2'))
+
+instance Arbitrary Expr where
+
+  arbitrary = sized gen_expr
+
+  shrink (Constr _ _)      = []
+  shrink (Observe l j e)   = e : (map (Observe l j) (shrink e))
+  shrink (Lambda n e)      = e : (map (Lambda n) (shrink e))
+  shrink (Apply e n)       = let alts = e : (map (flip Apply n) (shrink e))
+                             in case e of
+                               (Lambda _ e') -> e' : alts
+                               _             -> alts
+  shrink (Let (n,e1) e2)   = e2 : e1
+                             :    (map (Let (n,e1)) (shrink e2))
+                             ++   (map (\e-> Let (n,e) e2) (shrink e1))
+  shrink _                 = [c_0 []]
+
+--------------------------------------------------------------------------------
+-- Main
 
 main :: IO ()
 main = quickCheckWith args prop_actuallyFaulty
